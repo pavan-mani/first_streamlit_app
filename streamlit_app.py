6d0148495671498ff93a7ae6b2fe1c826d2e608c
pavan-mani <141003536+pavan-mani@users.noreply.github.com>
2023-07-31 17:40:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.text('🥣 Omega 3 &  Blueberry Oatmeal')
 streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard_Boiled Free_Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
