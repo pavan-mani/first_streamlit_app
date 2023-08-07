928bc8684a5b7c579c1a1c5c820a7e9474471507
pavan-mani <141003536+pavan-mani@users.noreply.github.com>
2023-08-07 19:46:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,6 @@ def get_fruitvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-expect URLError as e:
-  streamlit.error()
-
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
