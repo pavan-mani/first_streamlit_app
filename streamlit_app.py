ee52df40d9994d97984a081783f0f9be0fa00860
pavan-mani <141003536+pavan-mani@users.noreply.github.com>
2023-08-07 19:41:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,7 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
 
-
-streamlit.header("the fruit load list contains:")
+streamlit.header('the fruit load list contains:')
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
